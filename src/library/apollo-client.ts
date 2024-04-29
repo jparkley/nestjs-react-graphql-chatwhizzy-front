ba38176d4cfbcfca39cc4a1fd1768d05290695f4
jparkley <parkleydondon@yahoo.com>
2024-04-29 10:54:22 -0700

    add proxy for development

--- src/library/apollo-client.ts
+++ src/library/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: process.env.REACT_APP_GRAPHQL_URL,
+  uri: `${process.env.REACT_APP_GRAPHQL_URL}graphql`,
 });
 
 export default apolloClient;
