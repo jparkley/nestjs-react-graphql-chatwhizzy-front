bdeac7d993960635945b1358d77f2a4bc6cbd2de
jparkley <parkleydondon@yahoo.com>
2024-05-02 10:07:55 -0700

    add new routes

---
+++ src/components/pages/Chat.tsx
@@ -0,0 +1,11 @@
+const Chat = () => {
+  return (
+    <>
+      <div>
+        <h2>Home, Hello!</h2>
+      </div>
+    </>
+  );
+};
+
+export default Chat;
--- src/components/routes/Routes.tsx
+++ src/components/routes/Routes.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Chat from "../pages/Chat";
 import Login from "../auth/Login";
 import Signup from "../auth/Signup";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/chat",
+    element: <Chat />,
+  },
   {
     path: "/login",
     element: <Login />,
