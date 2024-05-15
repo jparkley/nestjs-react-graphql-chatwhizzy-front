afcac0be370798aee01feb64e824fa4de7a137ed
jparkley <parkleydondon@yahoo.com>
2024-05-15 10:37:01 -0700

    add apollo cache.modify after mutation

--- src/library/hooks/useCreateChat.ts
+++ src/library/hooks/useCreateChat.ts
@@ -1,8 +1,24 @@
 import { useMutation } from "@apollo/client";
 import { CreateChatDocument } from "../graphql/gql.documents";
+import { ChatFragment } from "../graphql/fragments";
 
 const useCreateChat = () => {
-  return useMutation(CreateChatDocument);
+  return useMutation(CreateChatDocument, {
+    update(cache, { data }) {
+      // data: response data from createChat
+      cache.modify({
+        fields: {
+          chats(existingChatsRef = []) {
+            const newChatRef = cache.writeFragment({
+              data: data?.createChat,
+              fragment: ChatFragment,
+            });
+            return [...existingChatsRef, newChatRef];
+          },
+        },
+      });
+    },
+  });
 };
 
 export default useCreateChat;
