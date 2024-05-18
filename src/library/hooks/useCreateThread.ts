c86909a148e229faca9c83c7857a4c4d246d7660
jparkley <parkleydondon@yahoo.com>
2024-05-17 20:21:44 -0700

    add apollo cache.modify after thread mutation

--- src/library/hooks/useCreateThread.ts
+++ src/library/hooks/useCreateThread.ts
@@ -1,8 +1,24 @@
 import { useMutation } from "@apollo/client";
 import { CreateThreadDocument } from "../graphql/gql.documents";
+import { ThreadFragment } from "../graphql/fragments";
 
 const useCreateThread = () => {
-  return useMutation(CreateThreadDocument);
+  return useMutation(CreateThreadDocument, {
+    update(cache, { data }) {
+      // data: response data from createThread
+      cache.modify({
+        fields: {
+          threads(existingThreadsRef = []) {
+            const newThreadRef = cache.writeFragment({
+              data: data?.createThread,
+              fragment: ThreadFragment,
+            });
+            return [...existingThreadsRef, newThreadRef];
+          },
+        },
+      });
+    },
+  });
 };
 
 export default useCreateThread;
